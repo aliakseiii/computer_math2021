4e2be429886929c735bab5b520c9043592eae0fe
Alex Slonski <unnmd0@gmail.com>
2021-02-25 18:23:25 +0300

    test prog was modified

--- lw1/hypothenuse.py
+++ lw1/hypothenuse.py
@@ -1,5 +1,11 @@
-import sys
+﻿import sys
 
-hypotenuse = (float(sys.argv[1])**2+float(sys.argv[2])**2)**(1/2)
+if int(sys.argv[1]) <= 0 or int(sys.argv[2]) <= 0:
+	print("Invalid data")
+else:
+	a = float(sys.argv[1])
+	b = float(sys.argv[2])
 
-print("Length of hypotenuse is", hypotenuse)
+	hypotenuse = (a**2+b**2)**(1/2)
+
+	print("Length of hypotenuse is", hypotenuse)
